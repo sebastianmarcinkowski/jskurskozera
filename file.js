6da089dc7218210eae4deab42d2e15ff095fefc0
Sebastian Marcinkowski <kontakt@sebastianmarcinkowski.pl>
2024-01-28 21:14:40 +0100

    8. Deklaracja zmiennych

--- file.js
+++ file.js
@@ -4,8 +4,36 @@ console.log("VSCode!");
 
 let test = "Hello world! TEST";
 
+const stala = "Nie może być zmieniona";
+
 console.log(test);
 
+// blok kodu
 {
     console.log(test);
+
+    console.log(zmienna);
+    // zmienna jest deklarowana niżej - hosting
+
+    var zmienna = "Globalna w danym pliku";
+    // jest to hosting, są hostowane czyli są dostępne przed deklaracją
+    // jako że są one dostępne globalnie, może to doprowadzić do problemów z zarządzaniem pamięcią i zależnościami między zmiennymi
+    // dlatego zaleca się używanie let i const
+
+    // nazwy zmiennych muszą zaczynać się od litery lub znaku podkreślenia, nie można używać spacji
+    // nie można używać słów kluczowych np. let, const, var, console, function itd.
+    // nie można używać znaków specjalnych np. !, @, #, $, %, ^, & itd.
+    // nie można używać polskich znaków
+    // nie można używać cyfr na początku nazwy zmiennej
+    // nazwy zmiennych są case sensitive, czyli wielkość liter ma znaczenie
+    // nazwy zmiennych powinny być pisane w języku angielskim
+    // nazwy zmiennych powinny być pisane w notacji camelCase
+    // nazwy zmiennych powinny być pisane w sposób opisowy
+    // nazwy zmiennych powinny być pisane w sposób jednoznaczny
+    // nazwy zmiennych powinny być pisane w sposób zwięzły
+    // nazwy zmiennych powinny być pisane w sposób konsekwentny
+    // nazwy zmiennych powinny być pisane w sposób zrozumiały dla innych osób
+    // nazwy zmiennych powinny być pisane w sposób zrozumiały dla przyszłych programistów
+    // nazwy zmiennych powinny być pisane w sposób zrozumiały dla Ciebie
+    // nazwy zmiennych powinny być pisane w sposób zrozumiały dla Twojego przyszłego ja
 }
