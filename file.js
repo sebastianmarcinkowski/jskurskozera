50f95c502a9aff6250aa1ce52f619daaddae2cf2
Sebastian Marcinkowski <kontakt@sebastianmarcinkowski.pl>
2024-01-28 23:06:27 +0100

    10. Typy danych

--- file.js
+++ file.js
@@ -36,4 +36,32 @@ console.log(test);
     // nazwy zmiennych powinny być pisane w sposób zrozumiały dla przyszłych programistów
     // nazwy zmiennych powinny być pisane w sposób zrozumiały dla Ciebie
     // nazwy zmiennych powinny być pisane w sposób zrozumiały dla Twojego przyszłego ja
+
+    /*
+
+    Komentarz blokowy
+
+    Ctrl + K + C - zakomentowanie zaznaczonego kodu
+    Ctrl + K + U - odkomentowanie zaznaczonego kodu
+    Ctrl + / - zakomentowanie/odkomentowanie zaznaczonego kodu
+
+    */
+
+    // Typy zmeinnych w JS - string, number, bigint, boolean, undefined, null, object, symbol
+
+    // string - ciąg (sekwencja) znaków, tekst, reprezentowane za pomocą pojedyńczych lub podwójnych cudzysłowów lub backticków
+
+    // number - liczba, reprezentowana za pomocą cyfr, mogą byc całkowite lub zmiennoprzecinkowe
+
+    // bigint - liczba całkowita o dowolnej precyzji, dopisujemy na końcu literał n, np. 10n
+
+    // boolean - wartość logiczna, true lub false
+
+    // undefined - wartość specjalna, oznacza brak wartości (jest zadeklarowana, nie ma przypisanej wartości)
+
+    // null - wartość specjalna, oznacza brak wartości lub brak obiektu, musi być jawnie przypisana do zmiennej aby wskazywać na brak wartości
+
+    // object - obiekt, reprezentuje złożone dane strukturalne, umożliwia grupowanie i przechowywanie danych w postaci klucz-wartość, jest to kolekcja właściwości, każda właściwość ma nazwę i wartość, właściwości mogą zawierać funkcje, które są wtedy nazywane metodami obiektu, deklaracja obiektu za pomocą nawiasów klamrowych {}, właściwości obiektu są oddzielone przecinkami, właściwości obiektu mogą być dostępne za pomocą kropki lub nawiasów kwadratowych, np. obiekt.wlasciwosc lub obiekt["wlasciwosc"], można zagnieżdżać obiekty w obiektach, np. obiekt.wlasciwosc.wlasciwosc, używany również jako typ danych dla tablic -> typeof => object, Daty to również obiekty
+    
+    // symbol - symbol, używany jako unikalny identyfikator dla obiektów, wprowadzony w ES6 (ECMAScript 6), tworzony za pomocą funkcji Symbol(), np. let id = Symbol(), zawsze tworzy nowy, unikalny symbol nawet jeśli użyszsz tej samej funkcji więcej niż raz, możemy również tworzyć symbole z opisem -> Symbol('opis')
 }
